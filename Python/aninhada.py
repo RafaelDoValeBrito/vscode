f792254586599e91491ec60befc1c5a38588308f
Rafael Do Vale <108241761+RafaelDoValeBrito@users.noreply.github.com>
2025-10-02 17:26:21 -0300

    Exe Emprestimo.py

--- Python/aninhada.py
+++ Python/aninhada.py
@@ -1,12 +1,11 @@
-casa = float(input("QUal o valor da casa que irá comprar? "))
-salario = float(input("QUal o seu salario? "))
+casa = float(input("Qual o valor da casa que irá comprar? "))
+salario = float(input("Qual o seu salario? "))
 anos = int(input("Em Quantos anos vai pagar? "))
-mensalidade = anos * 12
-prestacao = casa / mensalidade
-pretencao = salario * 0.30
+prestacao = casa / (anos * 12)
+minimo = salario * 30/100
 
 print("O valor da prestação será de {:.2f} ".format(prestacao))
-if prestacao > pretencao: 
+if prestacao <= minimo: 
     print("Emprestimo negado! ")
 else:
     print("Emprestimo aceito! ")
