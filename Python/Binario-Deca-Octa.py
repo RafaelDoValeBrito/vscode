00db849a0d0bdd48b91afdd32bbd2d916006be60
Rafael Do Vale <108241761+RafaelDoValeBrito@users.noreply.github.com>
2025-10-07 18:29:44 -0300

    Update Binario-Deca-Octa.py

--- Python/Binario-Deca-Octa.py
+++ Python/Binario-Deca-Octa.py
@@ -1,14 +1,14 @@
-num = int(input("Digite um número: ))
+num = int(input("Digite um número: "))
 print('''Escolha uma das bases para conversão:
 [1] Converter para binario
 [2] Converter para Decadeximal
 [3] Converter para Octa ''')
-opcao = int(input("Digite a opcão: )
+opcao = int(input("Digite a opcão: "))
 if opcao == 1:
-  print("{} Convertido para binário é igual {}.format(num, bin(num)[2:]))
+  print(" {} Convertido para binário é igual {}".format(num, bin(num)[2:]))
 elif opcao == 2:
-  print("{} Convertido para Hexadecimal é igual {}.format(num, hex(num)[2:]))
+  print("{} Convertido para Hexadecimal é igual {}".format(num, hex(num)[2:]))
 elif opcao == 3:
-  print("{} Convertido para Octadecimal é igual {}.format(num, oct(num)[2:]))
+  print("{} Convertido para Octadecimal é igual {}".format(num, oct(num)[2:]))
 else:
-  print(" Opção inválida! )
+  print(' Opção inválida! ')
