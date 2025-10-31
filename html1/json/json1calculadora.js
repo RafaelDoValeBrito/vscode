52f6395aac6203c293c6326bf50ba89c478e6110
Rafael Do Vale De Souza Brito <rafaelvalebrito@gmail.com>
2025-10-30 22:46:44 -0300

    Adiciona novos exemplos de HTML, CSS e JavaScript, incluindo flexbox, grid e pseudoclasses, além de melhorias na calculadora.

---
+++ html1/json/json1calculadora.js
@@ -0,0 +1,21 @@
+function somar (a, b) {
+    return a + b;
+}
+function subtrair (a, b) {
+    return a - b;
+}
+function multiplicar (a, b) {
+    return a * b;
+    
+} 
+function exponencial (a, b) {
+    return a ** b;
+}      
+function dividir (a, b) {
+    if (b === 0) 
+        alert("Erro: Divisão por zero!");   
+        return a/b;
+    } 
+    function exponencial (a, b) {
+        return math.pow(a, b);
+    }   
